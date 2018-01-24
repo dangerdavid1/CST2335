3f090914305987469cc1567efa1e5a723531f314
David <david_nguyen1@msn.com>
2018-01-24 17:47:28 -0500

    Added 3 activities for Lab2

---
+++ AndroidStudioProjects/AndroidLabs18W/app/src/main/java/com/example/david/androidlabs18w/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.david.androidlabs18w;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
